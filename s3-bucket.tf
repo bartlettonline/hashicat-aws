68f3eee6159260627b4ba22756c30e2655d136cc
bartlettonline <58756364+bartlettonline@users.noreply.github.com>
2021-10-13 16:18:04 +1100

    Update s3-bucket.tf

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,4 +2,12 @@ module "s3-bucket" {
   source  = "app.terraform.io/SBoptus/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
+  
+  bucket = "my-s3-bucket"
+  acl    = "private"
+
+  versioning = {
+    enabled = true
+  }
+
 }
